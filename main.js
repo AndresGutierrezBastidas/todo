f9258ed8dcd11a66e0c203bd2b087208fc07b6eb
Andrés Gutiérrez <65102015+AndresGutierrezBastidas@users.noreply.github.com>
2021-01-13 16:31:59 -0300

    Add files via upload
    actualización final

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
-const todos = [];
+const todos = JSON.parse(localStorage.getItem('todos')) || [];
 
+const render = () => {
+    const todoList = document.getElementById('todo-list');
+    const todosTemplate = todos.map(t => {return '<li>' + t + '</li>'});
+    todoList.innerHTML = todosTemplate.join('');
+    const elementos = document.querySelectorAll('#todo-list li');
+    elementos.forEach((elementos, i) =>{
+        elementos.addEventListener('click',() =>{
+            elementos.parentNode.removeChild(elementos);
+            todos.splice(i,1)
+            actualizaTodos(todos)
+            render()
+        })
+    })
+}
+const actualizaTodos = (todos) => {
+    const todoString = JSON.stringify(todos)
+    localStorage.setItem('todos', todoString)
+}
 window.onload = () =>{
+    render()
     const form = document.getElementById('todo-form');
     form.onsubmit = (e) =>{
         e.preventDefault();
@@ -8,8 +27,7 @@ window.onload = () =>{
         const todoText = todo.value;
         todo.value = '';
         todos.push(todoText);
-        const todoList = document.getElementById('todo-list');
-        const todosTemplate = todos.map(t => {return '<li>' + t + '</li>'});
-        todoList.innerHTML = todosTemplate.join('');
+        actualizaTodos(todos)
+        render();
     }
 }
